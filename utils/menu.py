e7175b3a4107934ac1c9a4e1dac2a970def29d33
UgniusMi <u.miliauskas@gmail.com>
2024-09-04 22:33:47 +0300

    pridejimui i duomenu baze pritaikyta tkinter browse file funkcija

--- utils/menu.py
+++ utils/menu.py
@@ -1,8 +1,8 @@
 from database.database import *
 from utils.predict_class import *
 from utils.graphs import show_classes_graph
-from models.model_cnn import start_training_model
-from models.model_rf import start_training
+from models.model_cnn import *
+from models.model_rf import start_training_rf
 import random
 from termcolor import colored
 from utils.helpers import *
@@ -26,8 +26,8 @@ def main_menu():
         print("1. Manage data ")
         print("2. Train model menu")
         print("3. Prediction menu")
-        print("4. TEST ")
-        print("5. Exit")
+        print("4. Open project overview document ")
+        print(colored("\n5. Exit", "red"))
         
 
         choice = input("\nPlease enter your choice: ")
@@ -39,20 +39,22 @@ def main_menu():
                 print("2. Add your photo to test_data.db")
                 print("3. Generate default train db records")
                 print("4. Generate default test db records")
-                print("5. Back to main menu")
+                print(colored("\n5. Back to main menu", "light_cyan"))
 
                 choice = input("\nPlease enter your choice: ")
                 if choice == '1':
-                    path = get_valid_image_path()
-                    classid = get_valid_classid()
-                    add_trainData(path, classid)
-                    print(colored(f"Your image with ({path}) is successfully added to train_data table", "light_green"))
+                    path = browse_file()
+                    if path:
+                        classid = get_valid_classid()
+                        add_trainData(path, classid)
+                        print(colored(f"Your image with ({path}) is successfully added to train_data table", "light_green"))
 
                 elif choice == '2':
-                    path = get_valid_image_path()
-                    classid = get_valid_classid()
-                    add_testData(path, classid)
-                    print(colored(f"Your image with path ({path}) is successfully added to test_data table", "light_green"))
+                    path = browse_file()
+                    if path:
+                        classid = get_valid_classid()
+                        add_testData(path, classid)
+                        print(colored(f"Your image with path ({path}) is successfully added to test_data table", "light_green"))
 
                 elif choice == '3':
                     table_name = 'train_data'
@@ -76,16 +78,16 @@ def main_menu():
                 print("1. Show Image classes graph")
                 print("2. Train CNN model")
                 print("3. Train Random forest model")
-                print("4. Back to main menu")
+                print(colored("\n4. Back to main menu", "light_cyan"))
 
                 choice = input("\nPlease enter your choice: ")
                 if choice == '1':
                     train_df, test_df = get_classes_from_test_and_train_data()
                     show_classes_graph(train_df, test_df)
                 elif choice == '2':
-                    start_training_model() # CNN
+                    start_training_cnn(learning_rate=LR, dropout_rate=DROP_RT, conv_filters=CONV_FILTER,dense_layer=DENSE_L, epochs=EPOCHS, batch_size=BATCH)
                 elif choice == '3':
-                    start_training() # random forest
+                    start_training_rf() # random forest
                 elif choice == '4':
                     print("You have returned to the main menu")
                     break  
@@ -97,23 +99,24 @@ def main_menu():
             while True:
                 print(colored("\nWelcome to prediction menu:\n", "light_magenta"))
 
-                model_path = 'savedmodels\\cnnTEST.keras'
+                model_path = 'savedmodels\\cnn982.keras' # cnn976.keras 42x42
                 model_path_rf ='savedmodels\\rfTEST.pkl'
                 print("1. Predict your image by path with CNN model")
                 print("2. Predict your image by path with Randomforest model")
                 print("3. Predict all test data with CNN model")
                 print("4. Show saved CNN model metrics")
-                print("5. Back to main menu")
+                print(colored("\n5. Back to main menu", "light_cyan"))
 
                 choice = input("\nPlease enter your choice: ")
                 if choice == '1':
-                    image_path = browse_file()
-                    # image_path = get_valid_image_path()
-                    cnn_prediction(image_path,model_path,classes)
-                
+                    image_path = browse_file()  
+                    if image_path:  
+                        cnn_prediction(image_path, model_path, classes)
+                        
                 elif choice == '2':
-                    image_path = get_valid_image_path()
-                    rf_prediction(image_path,model_path_rf,classes)
+                    image_path = browse_file()
+                    if image_path:
+                        rf_prediction(image_path,model_path_rf,classes)
 
                 elif choice == '3':
                     predict_all_test(model_path)
@@ -129,8 +132,9 @@ def main_menu():
                  print(colored("Invalid choice. Please try again.\n", "red"))
 
         elif choice == '4':
-            print("TEST DRIVE")
-           
+            doc_path = "datasets\project_overview.rtf"
+            open_word_doc(doc_path)
+
         elif choice == '5':
             print(colored("Exiting the program. Goodbye!", "dark_grey"))
             break  
